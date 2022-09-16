05a71937561ed97f0bf80bfd803b44eb456927b0
RickTh2407 <101653871+RickTh2407@users.noreply.github.com>
2022-09-16 13:45:23 +0200

    Update main.py

--- ButterBringer/ButterBringer/main.py
+++ ButterBringer/ButterBringer/main.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env pybricks-micropython
+from ev3dev.ev3 import *
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+from pybricks.ev3devices import (Motor, LargeMotor, MediumMotor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
+
 
 
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
@@ -15,6 +17,23 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 # Create your objects here.
 ev3 = EV3Brick()
 
+#Program writing for testing
+touchSens1 = TouchSensor(Port.S1)
+touchSens2 = TouchSensor(Port.S2)
+colourBlindBoi = ColorSensor(Port.S3)
+sonic = UltrasonicSensor(Port.S4)
+mediumBoi = MediumMotor(Port.A)
+bigBoi1 = LargeMotor(Port.B)
+bigBoi2 = LargeMotor(Port.C)
 
-# Write your program here.
-ev3.speaker.beep()
+Sound.tone(1500, 2000)
+
+while (touchSens1.value() == 1) or (touchSens2.value() == 1):
+    bigBoi1.run_forever(speed_sp = 20)
+    bigBoi2.run_forever(speed_sp = 20)
+    mediumBoi.run_forever(speed_sp = 20)
+
+bigBoi1.stop(stop_action='brake')
+bigBoi2.stop(stop_action='brake')
+mediumBoi.stop(stop_action='brake')
+
